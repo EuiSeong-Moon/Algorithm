ebb2b76c8a9802a418788017394b70ab9ff61ccc
Blue_Moon <moon7392@naver.com>
2019-02-04 22:20:43 +0900

    KMP

---
+++ SE_Expert/String.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+vector<int> getPi(string p) {
+	int m = (int)p.size();
+	int j = 0;
+	vector<int> pi(m, 0);
+	for (int i = 1; i < m; i++) {
+		while (j > 0 && p[i] != p[j]) {
+			j = pi[j - 1];
+		}
+		if (p[i] == p[j])
+			pi[i] = ++j;
+	}
+	return pi;
+}
+vector<int> kmp(string s, string p) {
+	vector<int> ans;
+	vector<int> pi = getPi(p);
+	int n = (int)s.size(), m = (int)p.size(), j = 0;
+	for (int i = 0; i < n; i++) {
+		while (j > 0 && s[i] != p[j])
+			j = pi[j - 1];
+		if (s[i] == p[j]) {
+			if (j == m - 1) {
+				ans.push_back(i - m + 1);
+				j = pi[j];
+			}
+			else {
+				j++;
+			}
+		}
+	}
+	return ans;
+}
+int main(void) {
+	for (int test = 0; test < 10; test++) {
+		int N;
+		cin >> N;
+		string finds;
+		cin >> finds;
+		string target;
+		cin >> target;
+		vector<int> answer = kmp(target, finds);
+
+		cout <<"#"<<N<<" "<< answer.size() << endl;
+	}
+	return 0;
+}
